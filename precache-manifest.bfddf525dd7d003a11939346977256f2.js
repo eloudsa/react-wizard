294c541461c164a38d0c0d1be10addc341c85bdb
eloudsa <said@noratek.net>
2020-04-27 13:35:42 +0200

    Updates

---
+++ precache-manifest.bfddf525dd7d003a11939346977256f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d34b8243d445281770f1e1148964966",
+    "url": "/react-wizard/index.html"
+  },
+  {
+    "revision": "86c0934f430842deba14",
+    "url": "/react-wizard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1af993ca6ad2a57c3b82",
+    "url": "/react-wizard/static/js/2.5851de84.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-wizard/static/js/2.5851de84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c0934f430842deba14",
+    "url": "/react-wizard/static/js/main.0f98ec2f.chunk.js"
+  },
+  {
+    "revision": "fa8822c76acb0ff72881",
+    "url": "/react-wizard/static/js/runtime-main.0ea2e9aa.js"
+  }
+]);
